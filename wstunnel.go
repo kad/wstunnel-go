d80f0d6e3885a515e49e6b56b9e0d268fe4ac57a
Alexander D. Kanevskiy <kad@kad.name>
2026-02-23 22:41:27 +0200

    split caddy integration to separate module

---
+++ wstunnel.go
@@ -0,0 +1,9 @@
+// Package wstunnel is a feature-complete Go implementation of wstunnel.
+//
+// This root package is primarily used to provide module-level information
+// and to satisfy tools that expect a Go package at the module root.
+// Most functionality is located in sub-packages under pkg/.
+package wstunnel
+
+// Version is the current version of wstunnel-go.
+const Version = "0.1.0"
